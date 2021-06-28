150cf02d2b451c9f1086b582fef8ec9414be2929
satyajit <satyajit.kumthekar@gmail.com>
2021-06-28 11:24:48 +0530

    tried to addtocart

--- src/pages/itemspage/items-page.component.jsx
+++ src/pages/itemspage/items-page.component.jsx
@@ -2,25 +2,31 @@ import React from 'react';
 import './items-page.styles.scss';
 import DATA from '../../components/data/data';
 import CustomButton from '../../components/custom-button/custom-button.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import { div } from 'prelude-ls';
 
 
 class Itemspage extends React.Component{
     constructor(){
         super();
-        this.state={data : DATA
+        this.state={data : DATA,
+            cart:[],
+            visible:false
         }
 
-         this.fooditem = items =>( items.find( item=> item.itemname === this.props.match.params.pagename));
+         this.foodItem = items =>( items.find( item=> item.itemname === this.props.match.params.pagename));
+
+         this.addToCart = item => {this.setState({cart:[...this.state.cart, item]})};
         
     }
 
     render(){
-        const {type, price, imageUrl} = this.state.data
+        const {type, price, imageUrl, subitems} = this.state.data
        
         return(
             <div className = 'food-item-card'>  
                 {
-                    this.fooditem(this.state.data).subitems.map(
+                    this.foodItem(this.state.data).subitems.map(
                     (subitem)=> 
                     <div className='card'>
 
@@ -31,7 +37,13 @@ class Itemspage extends React.Component{
                         <span className='type'>{subitem.type}</span>
                         <span className='type'> {subitem.price}</span>
                      </div>
-                     <CustomButton inverted={true} >Add to Cart</CustomButton>
+                     <CustomButton inverted={true} onClick = {(subitems)=>this.addToCart(subitems)}>Add to Cart</CustomButton>
+
+                     {
+                         this.state.visible ? <CartDropdown cartitems = {this.state.cart}/> : null
+                     }
+                     {console.log(this.state.cart)}
+                     
 
                     </div>
                     )   
